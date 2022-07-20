ee301acf4961ceae26c51a4981b3507a221a2709
Sudev Kumar <sudevkumar82@gmail.com>
2022-07-20 22:42:30 +0530

    Navbar

---
+++ Components/navbarContent.js
@@ -0,0 +1,106 @@
+let navbarContent = () => {
+  return `
+    <div id="top_navbar">
+        <div id="top_navbar_left">
+          <div class="hamberger">
+            <div id="hamberger1"></div>
+            <div id="hamberger2"></div>
+            <div id="hamberger3"></div>
+          </div>
+          <ul id="e_paper">
+            e-Paper
+          </ul>
+          <div class="vl"></div>
+          <ul id="sunday">
+            Sunday Chronical
+          </ul>
+          <div id="facebook_logo">
+          <a href="https://www.facebook.com/deccannews/"><img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSIDAcFHModkn3MBxQyxflCrWltiXdl1da13xOZWKMvyQbYSDeFe-EbzN_3YOZdNeXjGLo&usqp=CAU"
+          alt=""
+        /></a>
+          </div>
+          <div id="twiter_logo">
+          <a href="https://twitter.com/DeccanChronicle"><img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSzR04XT4mrm5PZ1fy17qUv_zI_4UucaiNvUQ&usqp=CAU"
+          alt=""
+        /></a>
+          </div>
+          <div id="insta_logo">
+          <a href="https://www.instagram.com/deccanchronicle_official/"><img
+          src="https://w7.pngwing.com/pngs/815/101/png-transparent-www-mevrouwpak-nl-advertising-graphic-design-instagram-instagram-logo-business-advertising.png"
+          alt=""
+        /></a>
+          </div>
+          <div id="deccan_logo">
+            <a href="https://www.instagram.com/deccanchronicle_official/"><img
+            src="https://www.deccanchronicle.com/images/app_store_iconWhite.png"
+            alt=""
+            /></a>
+            </div>
+        </div>
+
+        <div id="top_navbar_right">
+          <input type="checkbox" name="" id="" / checked = "checked">
+          <label for="">Auto Refresh</label>
+          <div class="vl-1"></div>
+          <ul>
+            Corporate Insolvency Resolution Process
+          </ul>
+        </div>
+      </div>
+
+      <div id="page_logo">
+      <a href=""><img
+      src="https://www.deccanchronicle.com/images/DeccanChronicle_Logo.webp"
+      alt=""
+    /></a>
+        <div id="data_time">
+          <p>Wednesday, Jul 20, 2022 | Last Update : 05:08 PM IST</p>
+        </div>
+      </div>
+
+      <div id="thick_line"></div>
+
+      <div id="nav_bar_content">
+      <a href=""><img
+      src="https://www.freepnglogos.com/uploads/logo-home-png/chimney-home-icon-transparent-1.png"
+      alt=""
+    /></a>
+        <ul>
+          <a href="">Nation</a>
+        </ul>
+        <ul>
+          <a href="">South</a>
+        </ul>
+        <ul>
+          <a href="">World</a>
+        </ul>
+        <ul>
+          <a href="">Entertainment</a>
+        </ul>
+        <ul>
+          <a href="">Job & Education</a>
+        </ul>
+        <ul>
+          <a href="">Sports</a>
+        </ul>
+        <ul>
+          <a href="">technology</a>
+        </ul>
+        <ul>
+          <a href="">Lifestyle</a>
+        </ul>
+        <ul>
+          <a href="">Gallery</a>
+        </ul>
+        <ul>
+          <a href="">More... &#8964;</a>
+        </ul>
+      </div>
+
+      <div id="thin_line"></div>
+    `;
+};
+
+export default navbarContent;
