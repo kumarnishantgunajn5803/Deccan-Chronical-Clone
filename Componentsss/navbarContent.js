e1822872866f6826f217b373454c24d86416e1ca
saurabhrana7699 <1304rishabhrana@gmail.com>
2022-07-24 12:07:20 +0530

    final

---
+++ Componentsss/navbarContent.js
@@ -0,0 +1,71 @@
+let navbarContent = () => {
+  return `
+  
+
+
+      <div id="page_logo">
+      <a href=""><img
+      src="https://www.deccanchronicle.com/images/DeccanChronicle_Logo.webp"
+      alt=""
+    /></a>
+        <div id="data_time">
+          <p>Wednesday, Jul 20, 2022 | Last Update : 05:08 PM IST</p>
+        </div>
+      </div>
+
+      <div id="thick_line"></div>
+
+      <div id="nav_bar_content">
+      <a href=""><img
+      src="https://www.freepnglogos.com/uploads/logo-home-png/chimney-home-icon-transparent-1.png"
+      alt=""
+    /></a>
+        <ul>
+          <a href="./nationnews.html">Nation</a>
+        </ul>
+        <ul>
+          <a href="./southnews.html">South</a>
+        </ul>
+        <ul>
+          <a href="./world.html">World</a>
+        </ul>
+        <ul>
+          <a href="./Entertainment.html">Entertainment</a>
+        </ul>
+        <ul>
+          <a href="">Job & Education</a>
+        </ul>
+        <ul>
+          <a href="./sports.html">Sports</a>
+        </ul>
+        <ul>
+          <a href="">technology</a>
+        </ul>
+        <ul>
+          <a href="">Lifestyle</a>
+        </ul>
+        <ul>
+          <a href="">Gallery</a>
+        </ul>
+        
+        <select name="" id="thor">
+               <option value="">More..</option>
+               <option value="">Business</option>
+               <option value="">Opinion </option>
+               <option value="">Sunday Chronical</option>
+               <option value="">Featured </option>
+               <option value="">Hydrabaad Chronical </option>
+               <option value="">Science </option>
+               <option value="">Editor's picks </option>
+               <option value="">Just In </option>
+               <option value="">Astroguide</option>
+
+             </select>
+        
+      </div>
+
+      <div id="thin_line"></div>
+    `;
+};
+
+export default navbarContent;
